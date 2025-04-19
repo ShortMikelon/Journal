0a4eb4921e4cab9bbf7c863d2a5d0deacec96735
Aset Kenes <maxim.satanov@pairnote.com>
2025-04-19 14:09:24 +0500

    Added new routes

--- lib/presentations/routes.dart
+++ lib/presentations/routes.dart
@@ -1,5 +1,8 @@
 class Routes {
   static const String splash = '/splash';
-  static const String home = '/';
-  static const String articleDetails = '/article-details';
+  static const String home = '/home';
+  static const String articleDetails = '/article/details';
+
+  static const String articleCreateSandbox = '/article/create/sandbox';
+  static const String articleCreateParameter = '/article/create/parameter';
 } 
